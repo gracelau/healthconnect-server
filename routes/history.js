9ad89cc31e1af0121a5a57dc0a5ba36a60ec9487
Grace Lau <grace@gracelau.ca>
2024-06-07 16:05:52 -0700

    Added PUT and POST routes for Add Appointment and POST Appointment

--- routes/history.js
+++ routes/history.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const fs = require ("fs");
+const fs = require("fs");
 const path = require('path');
-const appts = require("../data/appointments.json")
-const meds =require("../data/medications.json")
+const meds = require("../data/medications.json")
 const refs = require("../data/referrals.json")
-const port = process.env.PORT || process.argv[2] || 8080
+const port = process.env.PORT || process.argv[2] || 8080;
+const { v4: uuidv4 } = require('uuid');
 
-
-const dataPath = path.join(__dirname, 'appointments.json');
+const dataPath = path.join(__dirname, '../data/appointments.json');
 //middleware to check request body
 
-const requestValid = (req, res, next) =>  {
+const requestValid = (req, res, next) => {
   const inputValues = Object.values(req.body);
   const inputKeys = Object.keys(req.body);
 
   const reqFields = [
-    "provider",
-    "reason",
+    "Provider",
+    "Reason",
     "details",
-    "date",
+    "timestamp",
   ];
 
   if (
@@ -29,89 +28,96 @@ const requestValid = (req, res, next) =>  {
       error: "Missing required properties.",
     });
   }
-    //check if every value is truthy
+  //check if every value is truthy
   if (!inputValues.every((value) => !!value)) {
     return res.status(400).json({
       error: "Missing required properties.",
     });
   }
-// Validate date format (M/D/YYYY)
-const date = req.body.date;
-const dateRegex = /^(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01])\/\d{4}$/;
-
-if (!dateRegex.test(date)) {
-  return res.status(400).json({
-    error: "Invalid date format. Expected format: M/D/YYYY.",
-  });
-}
-
-next();
+  next();
 };
 
-
-
+function getAppts() {
+  return JSON.parse(fs.readFileSync(dataPath, { encoding: "utf8" })); 
+}
 
 // get all appointments in a list
 router.get("/history/appointments", (req, res) => {
-    res.json(appts);
+  res.json(getAppts());
+});
 
-    // console.log(appts)
-  });
+// get single appointment
+router.get('/history/appointments/:id', (req, res) => {
+  const appts = getAppts();
+  const found = appts.find((element) => element.id == req.params.id);
+  //console.log(req.params.id)
+  res.json(found);
+})
 
-  // get single appointment
-  router.get('/history/appointments/:id', (req,res) => {
-    const found = appts.find((element) => element.id == req.params.id);
-    console.log(req.params.id)
-    res.json(found);
-  })
+//PUT for single appointment (Edit)
 
-  //PUT for single appointment
+router.put('/history/appointments/:id', requestValid, async (req, res) => {
+  const update = req.body;
+  const { id } = req.params;
 
+  if (req.body.id) {
+    return res.status(400).json({ message: `You cannot update this id.` });
+  }
+  try {
+    const appointments = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+    const index = appointments.findIndex((a) => a.id === parseInt(id));
 
-  router.put('/history/appointments/:id', requestValid, async(req,res) => {
-    const update= req.body;
-    const{id} = req.params;
-
-    if (req.body.id) {
-      return res.status(400).json({ message: `You cannot update this id.` });
-    }
-    try {
-      const appointments = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
-      const index = appointments.findIndex((a) => a.id === parseInt(id));
-  
-      // return an error if the id was not found
-      if (index === -1) {
-        return res.status(404).json({
-          message: `Warehouse with ID ${id} not found`,
-        });
-      }
-  
-      // update the warehouse record
-      appointments[index] = { ...appointments[index], ...update };
-      fs.writeFileSync(dataPath, JSON.stringify(appointments, null, 2), 'utf-8');
-  
-      res.status(200).json(appointments[index]);
-    } catch (err) {
-      console.error('PUT request to /history/appointments/:id failed: ', err);
-      res.status(500).json({
-        message: 'Internal server error',
+    // return an error if the id was not found
+    if (index === -1) {
+      return res.status(404).json({
+        message: `Warehouse with ID ${id} not found`,
       });
     }
-    
-  });
 
-  
+    appointments[index] = { ...appointments[index], ...update };
+    fs.writeFileSync(dataPath, JSON.stringify(appointments, null, 2), 'utf-8');
 
-  router.get("/history/medications", (req, res) => {
-    res.json(meds);
-    console.log(meds)
-  })
+    res.status(200).json(appointments[index]);
+  } catch (err) {
+    console.error('PUT request to /history/appointments/:id failed: ', err);
+    res.status(500).json({
+      message: 'Internal server error',
+    });
+  }
 
-  router.get("/referrals", (req, res) => {
-    res.json(refs);
-    console.log(refs)
-  })
+});
 
-  
+//POST -  Add New Appointment 
+router.post('/history/appointments/', (req, res) => {
+  const { provider, reason, details, timestamp } = req.body;
+  const appointments = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+  const id= uuidv4();
 
-  module.exports = router;
+  const newAppt= {
+    id: id,
+    Provider:provider,
+    Reason:reason,
+    details,
+    timestamp,
+  };
+  appointments.push(newAppt);
+  fs.writeFileSync(dataPath, JSON.stringify(appointments, null, 2), 'utf-8');
+  res.json(appointments); 
+
+});
+
+
+router.get("/history/medications", (req, res) => {
+  res.json(meds);
+  console.log(meds)
+})
+
+router.get("/referrals", (req, res) => {
+  res.json(refs);
+  console.log(refs)
+})
+
+
+
+module.exports = router;
+
